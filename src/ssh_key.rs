7724526dbecff44d8c579f0acf323370fc71862a
s1ntaxe770r <cyberdev01@protonmail.com>
2022-12-25 22:49:39 +0100

    new functions yay

--- src/ssh_key.rs
+++ src/ssh_key.rs
@@ -1,35 +1,62 @@
-// use crate::client::CivoClient;
-// use reqwest::{Error,};
-// use serde::{Deserialize, Serialize};
+use crate::{client::CivoClient, errors};
+use serde::{Deserialize, Serialize};
+use reqwest::header::USER_AGENT;
+use crate::errors::HTTPError;
+use crate::client::SimpleResponse;
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ssh_key {
+    pub id: String,
+    pub name: String,
+    pub fingerprint: String,
+    pub public_key: String,
+    // String for now because i don't know the rust equivalent of time.Time
+    pub created_at: String,
+}
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ssh_key_request { 
+    name: String,
+    key: String
+}
+type SshKeys = Vec<ssh_key>;
 
-// #[derive(Serialize, Deserialize, Debug)]
-// pub struct ssh_key {
-//     pub id: String,
-//     pub name: String,
-//     pub fingerprint: String,
-//     pub public_key: String,
-//     // String for now because i don't know the rust equivalent of time.Time
-//     pub created_at: String,
-// }
+impl CivoClient {
+    pub async fn list_ssh_keys(&self) -> Result<SshKeys,HTTPError> {
+        let ssh_endpoint = self.prepare_client_url("/v2/sshkeys"); 
+        let req = self
+            .http_client
+            .get(ssh_endpoint)
+            .bearer_auth(&self.api_key)
+            .header("Accept", "Application/json")
+            .header("Content-Type", "application/json")
+            .query(&[("region", &self.region)])
+            .header(USER_AGENT, "civo-rs")
+            .send()
+            .await.unwrap();
+        if !req.status().as_u16() <=300 {
+            return Err(HTTPError::new(req.status().as_u16(),req.text().await.unwrap().as_str()));
+        }
+        let keys = req.json::<SshKeys>().await;
+        Ok(keys.unwrap())
+    }
 
-// type SshKeys = Vec<ssh_key>;
+    pub async fn  new_ssh_key(&self,key:ssh_key_request)-> Result<SimpleResponse,HTTPError>{
+        let ssh_endpoint = self.prepare_client_url("/v2/sshkeys"); 
+        let jsn_bdy =  serde_json::to_string(&key).unwrap();
+        let req = self.http_client
+        .post(ssh_endpoint)
+        .bearer_auth(&self.api_key)
+        .header("Accept", "Application/json")
+        .header("Content-Type", "application/json")
+        .header(USER_AGENT, "civo-rs")
+        .body(jsn_bdy)
+        .send()
+        .await.unwrap();
+        if req.status().as_u16() <= 300 {
+            return Err(HTTPError::new(req.status().as_u16(),req.text().await.unwrap().as_str()));
 
-// impl CivoClient {
-//     pub async fn list_ssh_keys(&self) -> Result<Vec<SshKeys>,Error> {
-//         let ssh_endpoint = self.prepare_client_url("/v2/sshkeys");
-//         let req = self
-//             .http_client
-//             .get(ssh_endpoint)
-//             .bearer_auth(&self.api_key)
-//             .header("Accept", "Application/json")
-//             .header("Content-Type", "application/json")
-//             .query(&[("region", &self.region)])
-//             .send()
-//             .await?;
-//         if !req.status().is_success() {
-//             Err(Error::from(reqwest::StatusCodeError::new(response.status())));
-//         }
-//             let keys = req.json::<SshKeys>().await?;
-//         Ok(())
-//     }
-// }
+        }
+        let resp = req.json::<SimpleResponse>().await;
+        Ok(resp.unwrap())
+    }
+    
+}
