f17c2c03139b804c32e02c00a3aa1292d0a5d186
s1ntaxe770r <cyberdev01@protonmail.com>
2022-12-25 22:48:22 +0100

    more error improvement

--- src/errors.rs
+++ src/errors.rs
@@ -3,21 +3,22 @@ use std::error::Error;
 
 #[derive(Debug)]
 pub struct HTTPError {
-    pub Code: i32,
-    pub Message: String,
+    pub code: u16,
+    pub message: String,
 }
 
-
 impl HTTPError {
-    pub fn new(code:i32,message: &str) -> HTTPError{
-        HTTPError { Code: code, Message: message.to_string() }
+    pub fn new(code:u16,message: &str) -> HTTPError{
+        HTTPError { code, message: message.to_string() }
     } 
 }
 
 impl fmt::Display for HTTPError{ 
 fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f,"HTTP error {} : {}",self.Code, self.Message)
+        write!(f,"HTTP error {} : {}",self.code, self.message)
     }
 }
 
 impl Error for HTTPError {}
+
+
