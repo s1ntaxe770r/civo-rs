934e924b7dd47559b8d88a32bdc0d5ff0ce963d6
s1ntaxe770r <cyberdev01@protonmail.com>
2022-12-30 22:11:33 +0100

    add updoot ssh-key func

--- src/ssh_key.rs
+++ src/ssh_key.rs
@@ -1,6 +1,5 @@
-use crate::client::SimpleResponse;
 use crate::errors::HTTPError;
-use crate::{client::CivoClient, errors};
+use crate::{client::CivoClient};
 use reqwest::header::USER_AGENT;
 use serde::{Deserialize, Serialize};
 #[derive(Serialize, Deserialize, Debug)]
@@ -63,7 +62,7 @@ impl CivoClient {
             .send()
             .await
             .unwrap();
-        if &req.status().as_u16() <= &300 {
+        if &req.status().as_u16() >= &300 {
             return Err(HTTPError::new(
                 req.status().as_u16().clone(),
                 req.text().await.unwrap().as_str(),
@@ -71,5 +70,32 @@ impl CivoClient {
         }
         let resp =  req.json::<SshKeyResponse>().await;
         Ok(resp.unwrap())
+    }
+
+    pub async fn update_ssh_key(&self,name: &str , ssh_key_id: &str) -> Result<ssh_key, HTTPError>  {
+        let ssh_endpoint = self.prepare_client_url("/v2/sshkeys");
+        // /v2/sshkeys/key_id
+        ssh_endpoint.join(ssh_key_id).unwrap();
+        let jsn_bdy = serde_json::to_string(name).unwrap();
+        let  req = self
+            .http_client
+            .put(ssh_endpoint)
+            .bearer_auth(&self.api_key)
+            .header("Accept", "Application/json")
+            .header("Content-Type", "application/json")
+            .header(USER_AGENT, "civo-rs")
+            .body(jsn_bdy)
+            .send()
+            .await
+            .unwrap();
+        if &req.status().as_u16() >= &300 {
+            return Err(HTTPError::new(
+                req.status().as_u16().clone(),
+                req.text().await.unwrap().as_str(),
+            ));
+        }
+        let resp =  req.json::<ssh_key>().await;
+        Ok(resp.unwrap())
+        // dbg!(req.text().await.unwrap())
     }
 }
